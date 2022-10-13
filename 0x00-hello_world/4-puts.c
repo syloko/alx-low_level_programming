b1f888fd8e8b35a16f4b65c873f4948d69cf9cea
syloko <sylvester.okoyomon@yahoo.com>
2022-10-13 13:48:35 -0700

    This is a puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - function prints out string and a trailing new line to stdout
+ * returns a nonnegative number if successful.
+ */
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);	
+}
