444314325be19dbaaa069f57775436c185870d6e
syloko <sylvester.okoyomon@yahoo.com>
2022-10-13 13:53:58 -0700

    This is a puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * main - function prints out string and a trailing new line to stdout
  * returns a nonnegative number if successful.
  */
-int main (void)
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
 	return (0);	
