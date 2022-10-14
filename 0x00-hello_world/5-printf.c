902c7082362a658aa242a2aa704b1095dffd2a61
syloko <sylvester.okoyomon@yahoo.com>
2022-10-14 00:10:29 -0700

    This is task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,9 @@
 
 
 /**
- *
- *  * main - prints the phrase with proper grammar,
- *
- *   * but the outcome is a piece of art,
- *
- *    * Return: 0 if exited properly, non-zero otherwise
- *
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
