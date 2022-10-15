13e25205b15345ceeed99269290ffcc0bb678d54
syloko <sylvester.okoyomon@yahoo.com>
2022-10-15 17:16:13 +0000

    A random value of integer is generated

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* Main displays a random number for n each time program is run
+* and begins on a new line.
+* Returns a value of (0) for each successful run
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+
+	printf("%d is positive\n", n);
+
+	else if (n == 0)
+
+	printf("%d is zero\n", n);
+
+	else if (n < 0) 
+
+	printf("%d is negative", n);
+
+	return (0);
+} 
