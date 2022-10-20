4fa14b6cbc8aceac3d0f84a1dd4e420a4701a38f
syloko <sylvester.okoyomon@yahoo.com>
2022-10-20 15:25:02 +0000

    task o

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void positive_or_negative(int i);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
