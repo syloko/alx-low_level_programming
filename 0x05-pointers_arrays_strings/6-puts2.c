e7e1f5a20058635cb932c384c507e45be7849c44
syloko <sylvester.okoyomon@yahoo.com>
2022-10-25 03:43:00 +0000

    added a few more comments on line 5

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,6 +3,7 @@
 /**
 * puts2 - prints every other character of a string, starting
 * with the first character, followed by a new line
+* @str: string to be printed
 * Return: void
 */
 void puts2(char *str)
